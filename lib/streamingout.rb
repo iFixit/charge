6eb2be4d199911cec571159f35602c11540c0605
Daryl Metzler <daryl@ifixit.com>
2018-12-14 14:24:56 -0700

    Drop streamingout
    This has been replaced by the Helpers version.

--- lib/streamingout.rb
+++
@@ -1,9 +0,0 @@
-module StreamingOutput
-   def stream_msg msg
-      @stream_out << "<p>#{msg}<p>"
-   end
-   
-   def stream_warning warning 
-      @stream_out << "<p><b>(!!!) WARNING: #{warning} (!!!)</b><p>"
-   end
-end
