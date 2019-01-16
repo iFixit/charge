967ce5da3201d8f19a0da090910023b5cdc4556a
Daryl Metzler <daryl@ifixit.com>
2019-01-16 12:43:30 -0700

    Add a basic curl
    Which turned out to fail hard! So wrap everything up in a nice safe way.
    Just print out the exception message if it fails, as it will be okay.

--- lib/charge/services/cache_buster.rb
+++ lib/charge/services/cache_buster.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'uri'
+
 module Charge
    module Services
       class CacheBuster
@@ -11,6 +14,15 @@ module Charge
 
             def bust_cache_on_host host, key
                puts "Cache bust '#{key}' on host '#{host}'"
+               delete_uri = URI.parse("https://#{host}/#{key}")
+               puts "Sending DELETE to: #{delete_uri}"
+               begin
+                  response = Net::HTTP.get_response(delete_uri)
+                  puts "DELETE response code: #{reponse.code}"
+               rescue StandardError => e
+                  puts "Delete failed for URI: #{delete_uri}"
+                  puts "ERROR: #{e.inspect}"
+               end
             end
          end
       end
