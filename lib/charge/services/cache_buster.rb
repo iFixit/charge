74d73abf89f02458d2009b870720c5b475c354de
Daryl Metzler <daryl@ifixit.com>
2019-01-16 14:11:23 -0700

    Skip SSL checking
    We don't have the world domination CA available for charge (it'd be a
    security hazard!) so let's skip verify (`curl -k`) when connecting to
    the dev machines.

--- lib/charge/services/cache_buster.rb
+++ lib/charge/services/cache_buster.rb
@@ -1,5 +1,6 @@
 require 'net/http'
 require 'uri'
+require 'openssl'
 
 module Charge
    module Services
@@ -16,9 +17,17 @@ module Charge
                puts "Cache bust '#{key}' on host '#{host}'"
                delete_uri = URI.parse("https://#{host}/#{key}")
                puts "Sending DELETE to: #{delete_uri}"
+               request = Net::HTTP::Delete.new(delete_uri)
+               req_options = {
+                  use_ssl: delete_uri.scheme == "https",
+                  verify_mode: OpenSSL::SSL::VERIFY_NONE,
+               }
                begin
-                  response = Net::HTTP.get_response(delete_uri)
-                  puts "DELETE response code: #{reponse.code}"
+                  response = Net::HTTP.start(delete_uri.hostname,
+                        delete_uri.port, req_options) do |http|
+                     http.request(request)
+                  end
+                  puts "DELETE response code: #{response.code}"
                rescue StandardError => e
                   puts "Delete failed for URI: #{delete_uri}"
                   puts "ERROR: #{e.inspect}"
